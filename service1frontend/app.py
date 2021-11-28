62f4d21373bb515348233d94151c092d237226f8
dkthecoder <dhruv.151@live.co.uk>
2021-11-28 15:29:04 +0000

    rearranging file structure

--- service1frontend/app.py
+++ service1frontend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, flash
 from forms import AddBlackCard, AddWhiteCard, LetsPlay
+import requests
 
 import blackcards, whitecards, magicmaker
 
