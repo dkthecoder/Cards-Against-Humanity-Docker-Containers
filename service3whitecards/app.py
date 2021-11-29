15068f914c0a66ba18b6f1cc70e1610f3f4a4989
dkthecoder <dhruv.151@live.co.uk>
2021-11-29 14:46:39 +0000

    troubleshooting nginx in docker deploy, fixed loading of black/white cards,

--- service3whitecards/app.py
+++ service3whitecards/app.py
@@ -1,19 +1,19 @@
 import pandas as pd
-from flask import Flask, request, Response
+from flask import Flask, request, Response, json
 
 app = Flask(__name__)
 app.config.update(DEBUG=True)
 
 
 print("importing white cards")
-df = pd.read_csv (r'data\CAH - White Cards.csv', header=None)
+df = pd.read_csv (r'CAH_White_Cards.csv', header=None)
 print("importing white card COMPLETE")
 
 
 #Return TRUE length of dataframe
 @app.route('/get_wc_length', methods=['GET'])
 def get_wc_length():
-    event_name =  len(df) - 1
+    event_name =  str(len(df) - 1)
     return Response(event_name, mimetype='text/plain')
 
 
@@ -22,28 +22,28 @@ def get_wc_length():
 @app.route('/read_all_wc', methods=['GET'])
 def read_all_wc():
     event_name = df.values.tolist()
-    return Response(event_name, mimetype='text/plain')
+    return Response(json.dumps(event_name), mimetype='application/json')
 
 
 #retrieve a card
 @app.route('/retrieve_wc/<card_id>', methods=['GET', 'POST'])
 def retrieve_wc(card_id):
-    event_name = df.loc[card_id].values.tolist()
-    return Response(event_name, mimetype='text/plain')
+    event_name = df.loc[int(card_id)].values.tolist()
+    return Response(json.dumps(event_name), mimetype='application/json')
 
 
 #delete card
 #COULD SET TO POST
 @app.route('/delete_wc/<card_id>', methods=['GET', 'POST'])
 def delete_bwc(card_id):
-    df.drop(df.index[(df[0] == card_id)], axis=0)
+    df.drop(df.index[(df[0] == int(card_id))], axis=0)
 
 
 #add card
 #COULD SET TO POST
 @app.route('/add_wc/<card>', methods=['GET', 'POST'])
 def add_wc(card):
-    df.loc[len(df)] = card
+    df.loc[len(df)] = str(card)
 
 
 if __name__ == '__main__':
