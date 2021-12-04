114e3af7374f417bc4f5e5884d1424fbc461842f
dkthecoder <dhruv.151@live.co.uk>
2021-12-04 13:44:08 +0000

    service one updates

--- service1frontend/app.py
+++ service1frontend/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, flash
-from requests.api import request
+from flask import Flask, render_template, url_for, redirect, flash, json, request
 from forms import AddBlackCard, AddWhiteCard, LetsPlay
 import requests
 
